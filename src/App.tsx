42ebe01170d0e971bc65dd9266adcfbe915a360c
Ranjit Rautaray <ranjit.r@terralogic.com>
2023-08-10 11:23:40 +0530

    some basic data structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+// fibonacci number 
+
+
+function fibo(n:any){
+let arr = [0,1]
+for(var i= 2 ;i<= n ; i++ ){
+ arr[i] = (arr[i-1] +  arr[i-2])
+  console.log(arr)
+}
+ 
+}
+fibo(6)
+
+// factorial testing 
+const fact = (n:any)=>{
+  let result = 1;
+  for(let i = 2 ; i<=n ;i++){
+    result = result * i
+  }
+  console.log(result)
+
+}
+fact(8)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center text-success">this is heading</h1>
     </div>
   );
 }
